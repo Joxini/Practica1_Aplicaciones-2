93a19c01ca90974e4e9b8420dc9381fe97eaf7f6
Joxini <jocksanvargas15@gmail.com>
2024-01-29 20:42:40 -0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Index from './android/app/src/views/index';
+
+const App = () => {
+  return (
+    <View>
+      <Index />
+    </View>
+  );
+};
+
+export default App;
